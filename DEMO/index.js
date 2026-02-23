0f69f99cde1554a7e01a31146a8c4fb4c950e57b
Spaceman974 <baptiste.leblanc4@gmail.com>
2026-02-23 14:21:36 +0100

    nouvelle orga

--- DEMO/index.js
+++
@@ -1,7 +0,0 @@
-//Importer les librairies exportées du fichier calculatrice.js
-
-// import {add, div, sub, mult} from "./calculatrice.js"; quand on est sur un framework ou qu'on utilise des modules
-
-const calc = require("./calculatrice.js")
-calc.add(20, 32)
-calc.div(2, 0)
